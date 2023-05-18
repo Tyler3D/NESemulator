9a79445dcad05df10fc89cb2fbf230d7303164c0
jjl1409 <47550942+jjl1409@users.noreply.github.com>
2023-05-18 17:52:42 -0400

    asdasdasd

--- controller.c
+++ controller.c
@@ -42,10 +42,10 @@ void poll_controllers(uint8_t *data) {
     //    CLEAR_START_BUTTON(player1.buttons);
     //    SET_RIGHT_BUTTON(player1.buttons);
     //}
-    // if (ppu.framecount < 300){
-    //     SET_A_BUTTON(player1.buttons);
-    //     SET_RIGHT_BUTTON(player1.buttons);
-    // }
+    if (ppu.framecount < 300){
+        SET_A_BUTTON(player1.buttons);
+        SET_RIGHT_BUTTON(player1.buttons);
+    }
     // else{
     //     CLEAR_A_BUTTON(player1.buttons);
     // }
