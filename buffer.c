95f08ccc4c69f46dd337076abec63e937f9bd649
Tyler Manrique <Tyler.manrique@columbia.edu>
2023-05-17 21:29:43 -0400

    Sprite implements

--- buffer.c
+++ buffer.c
@@ -11,16 +11,18 @@ pixel convert_rgb(uint8_t hue, uint8_t value, uint8_t prio) {
 	return rgb;
 }
 
-void pixel_to_buffer(pixel pixel, uint16_t x, uint16_t y) {
-	//if (buffer[y * SCREEN_WIDTH + x].prio <= pixel.prio){
-		buffer[y * SCREEN_WIDTH + x] = pixel;
-	//}
+void pixel_to_buffer(pixel *pixel, uint16_t x, uint16_t y) {
+	if (x > 255 || y > 240)
+		return;
+	if (buffer[y * SCREEN_WIDTH + x].prio <= pixel->prio) {
+		buffer[y * SCREEN_WIDTH + x] = *pixel;
+	}
 }
 
 // Hardcoded 8x8 tile get that puts it into buffer
 // Palette not implemented
 // Rotation not yet implemented
-void tile_to_buffer(uint16_t addr, uint8_t palette, uint8_t prio, uint8_t x, uint8_t y, uint8_t rotation) {
+void tile_to_buffer(uint16_t addr, uint8_t palette, uint8_t prio, uint16_t x, uint16_t y, uint8_t rotation) {
 	uint8_t x_offset = 0;
 	uint8_t y_offset = 0;
 	for (int i = addr; i < addr + 8; i++) {
@@ -44,7 +46,7 @@ void tile_to_buffer(uint16_t addr, uint8_t palette, uint8_t prio, uint8_t x, uin
 			// Note palette is set to 0 for temporary purposes
 			//if (value > 0) {
 			pixel rgb = convert_rgb(0, value, prio);
-			pixel_to_buffer(rgb, x + x_offset, y + y_offset);
+			pixel_to_buffer(&rgb, x + x_offset, y + y_offset);
 			//}
 			x_offset++;
 		}
@@ -81,21 +83,16 @@ void tile_to_buffer(uint16_t addr, uint8_t palette, uint8_t prio, uint8_t x, uin
 // Also for 16x8 mode we use size; the selection of tile/rotation differs
 void sprite_to_buffer(uint8_t y, uint8_t id, uint8_t attributes, uint8_t x) {
 	uint16_t sprite_addr = (id << 4) + (SPRITE_PATTERN_ADDR ? 0x1000 : 0x0000);
-	printf("Sprite addr %X\n", sprite_addr);
 	uint8_t rotation = attributes >> 6;
 	uint8_t priority = (attributes >> 5) & 0x1;
-	printf("Sprite addr %X\n", sprite_addr);
-	tile_to_buffer(sprite_addr, attributes & 0x3, (priority == 0) ? -1 : 1, x, y, rotation);
+	tile_to_buffer(sprite_addr, attributes & 0x3, (priority == 1) ? 0 : 2, x, y, rotation);
 }
 
 void oam_to_buffer() {
 	log_oam();
 	fflush(logfp);
 	for (int i = 0; i < 64; i++) {
-		if (ppu.OAM[i].id == 0xa2) {
-			printf("Printing 0xA2\n");
-			sprite_to_buffer(ppu.OAM[i].y, ppu.OAM[i].id, ppu.OAM[i].attributes, ppu.OAM[i].x);
-		}
+		sprite_to_buffer(ppu.OAM[i].y, ppu.OAM[i].id, ppu.OAM[i].attributes, ppu.OAM[i].x);
 	}
 }
 
